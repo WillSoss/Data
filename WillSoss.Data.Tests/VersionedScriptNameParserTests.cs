7af82a74842b2d2ec90e1c871102ab6b5e38c177
willsoss <willsossamon@gmail.com>
2023-08-16 17:25:27 -0500

    Fix: Allow script names with spaces

--- WillSoss.Data.Tests/VersionedScriptNameParserTests.cs
+++ WillSoss.Data.Tests/VersionedScriptNameParserTests.cs
@@ -15,16 +15,16 @@ namespace WillSoss.Data.Tests
                 "6- _ six.sql",
                 "5.6.7.8-five.sql",
                 "5.6.7-four.sql",
-                "5.3 three.sql",
-                "4.1 two.sql",
-                "1 1.sql"
+                "5.3 thr-e_e.sql",
+                "4.1 2 two.sql",
+                "1 Create Schema.sql"
             };
 
             var expected = new (string version, string file)[]
             {
-                ("1", "1"),
-                ("4.1", "two"),
-                ("5.3", "three"),
+                ("1", "Create Schema"),
+                ("4.1", "2 two"),
+                ("5.3", "thr-e_e"),
                 ("5.6.7", "four"),
                 ("5.6.7.8", "five"),
                 ("6", "six"),

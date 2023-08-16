ee421fbf6433cbdd4cb8e3113af2320901555242
willsoss <willsossamon@gmail.com>
2023-08-16 18:09:06 -0500

    Feat: Support for Azure SQL databases

--- WillSoss.Data.Sql/drop.sql
+++ WillSoss.Data.Sql/drop.sql
@@ -1,12 +1,3 @@
-if (serverproperty('edition') = N'SQL Azure')
-begin
-    exec('drop database if exists [{database}]');
-end;
-else
-begin
-    if not exists (select 1 from sys.sysdatabases where name = '{{database}}')
-    begin
-        exec('alter database [{{database}}] set single_user with rollback immediate;
-        drop database [{{database}}];');
-    end;
-end;
+alter database [{{database}}] set single_user with rollback immediate;
+
+drop database [{{database}}];

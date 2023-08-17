5dec39372ecf54860e5cc8037213e832ba4a6435
William D. Sossamon <will@runly.io>
2023-08-17 13:43:51 -0500

    Feat: Add SQL testcontainers for integratio tests

---
+++ WillSoss.Data.Tests/MigrationSetupTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WillSoss.Data.Tests
+{
+    public class IntegrationTests : IClassFixture<IntegrationTestFixture>
+    {
+        private readonly IntegrationTestFixture Fixture;
+
+        public IntegrationTests(IntegrationTestFixture fixture)
+        {
+            Fixture = fixture;
+        }
+    }
+}

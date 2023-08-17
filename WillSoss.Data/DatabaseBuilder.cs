fb4de86ce626108520cc536b3e72545d1cad3155
William D. Sossamon <will@runly.io>
2023-08-17 14:49:58 -0500

    Refactor: Fluent builder ideas

---
+++ WillSoss.Data/DatabaseBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace WillSoss.Data
+{
+    public class DatabaseBuilder
+    {
+        readonly Func<DatabaseBuilder, Database> _build;
+        public DatabaseBuilder(Func<DatabaseBuilder, Database> build) 
+        {
+            _build = build;
+        }
+
+        public DatabaseBuilder AddThing() => this;
+    }
+}

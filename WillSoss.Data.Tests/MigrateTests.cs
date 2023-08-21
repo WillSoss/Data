d5af98ca573f2e422e1524b0580e6ee188b84823
William Sossamon <will@willsossamon.com>
2023-08-21 10:20:18 -0500

    Feat: Prevent running migrations out of order

--- WillSoss.Data.Tests/MigrateTests.cs
+++ WillSoss.Data.Tests/MigrateTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using System.CommandLine.Parsing;
 using WillSoss.Data.Sql;
 
 namespace WillSoss.Data.Tests
@@ -32,6 +33,111 @@ namespace WillSoss.Data.Tests
 
             // Assert
             db.Migrations.Count().Should().Be(4);
+        }
+
+        [Fact]
+        public async void ShouldApplyMigrations()
+        {
+            // Arrange
+            var migrationsPath = Path.Combine(Directory.GetCurrentDirectory(), "Scripts");
+
+            var cs = new SqlConnectionStringBuilder(_fixture.DbContainer.ConnectionString)
+            {
+                InitialCatalog = "test"
+            };
+
+            var db = SqlDatabase.CreateBuilder()
+                .WithConnectionString(cs.ToString())
+                .AddMigrations(migrationsPath)
+                .Build();
+
+            await db.Drop();
+
+            await db.Create();
+
+            // Act
+            var count = await db.MigrateToLatest();
+
+            // Assert
+            count.Should().Be(4);
+
+            var migrations = await db.GetAppliedMigrations(db.GetConnection());
+
+            // 4 scripts + database create
+            migrations.Count().Should().Be(5);
+        }
+
+        [Fact]
+        public async void ShouldApplyMigrationToVersion()
+        {
+            // Arrange
+            var migrationsPath = Path.Combine(Directory.GetCurrentDirectory(), "Scripts");
+
+            var cs = new SqlConnectionStringBuilder(_fixture.DbContainer.ConnectionString)
+            {
+                InitialCatalog = "test"
+            };
+
+            var db = SqlDatabase.CreateBuilder()
+                .WithConnectionString(cs.ToString())
+                .AddMigrations(migrationsPath)
+                .Build();
+
+            await db.Drop();
+
+            await db.Create();
+
+            await db.MigrateTo(new Version(1, 0));
+
+            // Act
+            var count = await db.MigrateToLatest();
+
+            // Assert
+            count.Should().Be(3);
+
+            var migrations = await db.GetAppliedMigrations(db.GetConnection());
+
+            // 4 scripts + database create
+            migrations.Count().Should().Be(5);
+        }
+
+        [Fact]
+        public async void ShouldNotApplySkippedMigrations()
+        {
+            // Arrange
+
+            // Apply migrations, but skip "1.1 one-one.sql"
+            var migrationsPath = Path.Combine(Directory.GetCurrentDirectory(), "Scripts - One Missing");
+
+            var cs = new SqlConnectionStringBuilder(_fixture.DbContainer.ConnectionString)
+            {
+                InitialCatalog = "test"
+            };
+
+            var db = SqlDatabase.CreateBuilder()
+                .WithConnectionString(cs.ToString())
+                .AddMigrations(migrationsPath)
+                .Build();
+
+            await db.Drop();
+
+            await db.Create();
+
+            await db.MigrateToLatest();
+
+            // Now set up for migrating again, but with the missing script
+            migrationsPath = Path.Combine(Directory.GetCurrentDirectory(), "Scripts");
+
+            db = SqlDatabase.CreateBuilder()
+                .WithConnectionString(cs.ToString())
+                .AddMigrations(migrationsPath)
+                .Build();
+
+            // Act
+            var ex = await Assert.ThrowsAsync<MigrationsNotAppliedInOrderException>(db.MigrateToLatest);
+
+            // Assert
+            ex.Should().NotBeNull();
         }
     }
 }
--- WillSoss.Data/DatabaseBuilder.cs
+++ WillSoss.Data/DatabaseBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http.Headers;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace WillSoss.Data
 {

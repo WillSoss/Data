b9827672e2ef9c8ab3bb06263ba629ed988c0899
William Sossamon <3278433+WillSoss@users.noreply.github.com>
2025-02-02 09:58:10 -0600

    WithConnectionStringName method/--name cli option for adding a connection string by name

--- WillSoss.DbDeploy/Cli/CliCommands.cs
+++ WillSoss.DbDeploy/Cli/CliCommands.cs
@@ -1,19 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.CommandLine;
+using System.Xml.Linq;
 
 namespace WillSoss.DbDeploy.Cli
 {
     internal static class CliCommands
     {
-        internal static Command Drop(IServiceCollection services)
+        internal static Command Drop(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("drop", "Drops the database if it exists."); ;
 
             command.AddOption(CliOptions.Unsafe);
 
-            command.SetHandler((cs, @unsafe) => services.AddTransient<ICliCommand>(s => new DeployCommand(
+            command.SetHandler((cs, name, @unsafe) => services.AddTransient<ICliCommand>(s => new DeployCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs,
+                cs ?? GetConnectionString(context, name),
                 null,
                 true,
                 @unsafe,
@@ -22,21 +25,21 @@ namespace WillSoss.DbDeploy.Cli
                 false,
                 false,
                 false
-                )), CliOptions.ConnectionString, CliOptions.Unsafe);
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName, CliOptions.Unsafe);
 
             return command;
         }
 
-        internal static Command Create(IServiceCollection services)
+        internal static Command Create(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("create", "Creates the database if it does not exist."); ;
 
             command.AddOption(CliOptions.Drop);
             command.AddOption(CliOptions.Unsafe);
 
-            command.SetHandler((cs, drop, @unsafe) => services.AddTransient<ICliCommand>(s => new DeployCommand(
+            command.SetHandler((cs, name, drop, @unsafe) => services.AddTransient<ICliCommand>(s => new DeployCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs,
+                cs ?? GetConnectionString(context, name),
                 null,
                 drop,
                 @unsafe,
@@ -45,12 +48,12 @@ namespace WillSoss.DbDeploy.Cli
                 false,
                 false,
                 false
-                )), CliOptions.ConnectionString, CliOptions.Drop, CliOptions.Unsafe);
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName, CliOptions.Drop, CliOptions.Unsafe);
 
             return command;
         }
 
-        internal static Command Migrate(IServiceCollection services)
+        internal static Command Migrate(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("migrate", "Migrates to the specified version, or latest if no version is specified."); ;
 
@@ -59,9 +62,9 @@ namespace WillSoss.DbDeploy.Cli
             command.AddOption(CliOptions.Pre);
             command.AddOption(CliOptions.Post);
 
-            command.SetHandler((cs, version, applyMissing, pre, post) => services.AddTransient<ICliCommand>(s => new DeployCommand(
+            command.SetHandler((cs, name, version, applyMissing, pre, post) => services.AddTransient<ICliCommand>(s => new DeployCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs,
+                cs ?? GetConnectionString(context, name),
                 version,
                 false,
                 false,
@@ -70,12 +73,12 @@ namespace WillSoss.DbDeploy.Cli
                 applyMissing,
                 pre,
                 post
-                )), CliOptions.ConnectionString, CliOptions.Version, CliOptions.ApplyMissing, CliOptions.Pre, CliOptions.Post);
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName, CliOptions.Version, CliOptions.ApplyMissing, CliOptions.Pre, CliOptions.Post);
 
             return command;
         }
 
-        internal static Command Deploy(IServiceCollection services)
+        internal static Command Deploy(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("deploy", "Creates the database if it does not exist, then migrates to latest."); ;
 
@@ -86,9 +89,9 @@ namespace WillSoss.DbDeploy.Cli
             command.AddOption(CliOptions.Pre);
             command.AddOption(CliOptions.Post);
 
-            command.SetHandler((cs, version, drop, @unsafe, applyMissing, pre, post) => services.AddTransient<ICliCommand>(s => new DeployCommand(
+            command.SetHandler((cs, name, version, drop, @unsafe, applyMissing, pre, post) => services.AddTransient<ICliCommand>(s => new DeployCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs,
+                cs ?? GetConnectionString(context, name),
                 version,
                 drop,
                 @unsafe,
@@ -97,24 +100,24 @@ namespace WillSoss.DbDeploy.Cli
                 applyMissing,
                 pre,
                 post
-                )), CliOptions.ConnectionString, CliOptions.Version, CliOptions.Drop, CliOptions.Unsafe, CliOptions.ApplyMissing, CliOptions.Pre, CliOptions.Post);
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName, CliOptions.Version, CliOptions.Drop, CliOptions.Unsafe, CliOptions.ApplyMissing, CliOptions.Pre, CliOptions.Post);
 
             return command;
         }
 
-        internal static Command Status(IServiceCollection services)
+        internal static Command Status(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("status", "Displays the migration status of the database.");
 
-            command.SetHandler((cs) => services.AddTransient<ICliCommand>(s => new StatusCommand(
+            command.SetHandler((cs, name) => services.AddTransient<ICliCommand>(s => new StatusCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs
-                )), CliOptions.ConnectionString);
+                cs ?? GetConnectionString(context, name)
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName);
 
             return command;
         }
 
-        internal static Command Run(IServiceCollection services)
+        internal static Command Run(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("run", "Executes a script or action."); ;
 
@@ -125,28 +128,36 @@ namespace WillSoss.DbDeploy.Cli
 
             command.AddArgument(arg);
 
-            command.SetHandler((cs, action) => services.AddTransient<ICliCommand>(s => new RunCommand(
+            command.SetHandler((cs, name, action) => services.AddTransient<ICliCommand>(s => new RunCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs,
+                cs ?? GetConnectionString(context, name),
                 action
-                )), CliOptions.ConnectionString, arg);
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName, arg);
 
             return command;
         }
 
-        internal static Command Reset(IServiceCollection services)
+        internal static Command Reset(HostBuilderContext context, IServiceCollection services)
         {
             var command = new Command("reset", "Runs the reset script on the database. Can be used to clean up data after test runs."); ;
 
             command.AddOption(CliOptions.Unsafe);
 
-            command.SetHandler((cs, @unsafe) => services.AddTransient<ICliCommand>(s => new ResetCommand(
+            command.SetHandler((cs, name, @unsafe) => services.AddTransient<ICliCommand>(s => new ResetCommand(
                 s.GetRequiredService<DatabaseBuilder>(),
-                cs,
+                cs ?? GetConnectionString(context, name),
                 @unsafe
-                )), CliOptions.ConnectionString, CliOptions.Unsafe);
+                )), CliOptions.ConnectionString, CliOptions.ConnectionStringName, CliOptions.Unsafe);
 
             return command;
+        }
+
+        private static string? GetConnectionString(HostBuilderContext context, string? name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return null;
+
+            return context.Configuration.GetConnectionString(name);
         }
     }
 }
